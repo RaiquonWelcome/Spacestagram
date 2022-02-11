d790121ad13b6647cf9cc5bf9dd981b992b0410e
raiquonwelcome <35848176+RaiquonWelcome@users.noreply.github.com>
2022-02-11 00:27:41 -0500

    Updated app.js
    Added NASA api key to server. Attempted to make a get request to NASA api.

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express')
+const logger = require('morgan')
 const app = express()
 const port = 3000
 
-app.use(express.static('views'))
+const API_KEY = 'Ze29b4Ox5ISqYeaf848AmS8nkDn1gCzGHrbyp5lR'
 
 app.set('view engine', 'ejs')
 
+app.use(express.static('views'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+app.use(logger('dev'))
+
 app.get('/', (req, res) => {
-    res.render("index.ejs")
+    res.render("index.ejs") 
+    console.log('here')
+})
+
+app.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`, (req, res) => {
+    console.log(res.body)
+    console.log('here')
 })
 
 app.listen(port)
