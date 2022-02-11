3ad503ae48a45d686cab6c750d309e03013aae5f
raiquonwelcome <35848176+RaiquonWelcome@users.noreply.github.com>
2022-02-10 23:21:28 -0500

    Updated app.js and Created index.ejs
    1) Updated app.js to be a server created with express
    
    2) Changed index.html into an ejs file

--- app.js
+++ app.js
@@ -1,66 +1,13 @@
-const http = require('http');
-const port = 3000;
-const fs = require('fs');
-const url = require('url');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const ROOT_DIR = 'public';
+app.use(express.static('views'))
 
-const MIME_TYPES = {
-    css: "text/css",
-    gif: "image/gif",
-    htm: "text/html",
-    html: "text/html",
-    ico: "image/x-icon",
-    jpeg: "image/jpeg",
-    jpg: "image/jpeg",
-    js: "application/javascript",
-    json: "application/json",
-    png: "image/png",
-    txt: "text/plain"
-  };
-   
+app.set('view engine', 'ejs')
 
-function get_mime(filename) {
-    let type
-    for (let ext in MIME_TYPES) {
-      type = MIME_TYPES[ext]
-      if (filename.indexOf(ext, filename.length - ext.length) !== -1) {
-        return type
-      }
-    }
-    return MIME_TYPES["txt"]
-  }
+app.get('/', (req, res) => {
+    res.render("index.ejs")
+})
 
-const server = http.createServer(function(req, res) {
-
-    let urlObj = url.parse(req.url, true, false);
-    let filePath = ROOT_DIR + urlObj.pathname;
-
-    console.log('\n============================');
-	console.log("PATHNAME: " + urlObj.pathname);
-    console.log("REQUEST: " + filePath);
-    console.log("METHOD: " + req.method);
-
-    if(urlObj.pathname === '/') filePath = ROOT_DIR + '/index.html';
-    
-    fs.readFile(filePath, function(err, data) {
-        if(err) {
-            res.writeHead(404);
-            res.write(err.message);
-            res.end();
-        }else{
-            res.writeHead(200, {'Content-Type': get_mime(filePath)});
-            res.end(data);
-        }
-
-        
-       
-        console.log('done');
-    });
-
-    
-
-}).listen(port);
-
-console.log('Server Running at Port 3000 CNTL-C to quit');
-console.log('To test with browser, visit http://localhost:3000');
+app.listen(port)
