86825b0cf2f00079aa51a5ac5473094e5c448b35
raiquonwelcome <35848176+RaiquonWelcome@users.noreply.github.com>
2022-02-17 13:49:09 -0500

    Updated directory

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const path = require('path');
+const favicon = require('serve-favicon');
 const logger = require('morgan')
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const https = require('https')
 
 const app = express()
@@ -10,13 +14,17 @@ const API_KEY = 'Ze29b4Ox5ISqYeaf848AmS8nkDn1gCzGHrbyp5lR'
 
 
 
-
+app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'ejs')
 
-app.use(express.static('views'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'))
+app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true}))
+
 
 app.get('/', (req, res) => {
     
