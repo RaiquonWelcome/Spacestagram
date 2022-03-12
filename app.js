9cb5c7438f2e0a2a7fb1da7498d9ecc19ddf32fd
raiquonwelcome <35848176+RaiquonWelcome@users.noreply.github.com>
2022-03-11 22:56:50 -0500

    Updated app.js
    1) Encrypted the password when user registers
    
    2) Created database for users and user info is added to collection when registered

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const https = require('https')
-const MongoClient = require('mongodb').mongoClient
+const { MongoClient } = require('mongodb')
 const bcrypt = require('bcrypt')
 
 //Constant Variables
 const app = express()
 const port = 3000
 const API_KEY = 'Ze29b4Ox5ISqYeaf848AmS8nkDn1gCzGHrbyp5lR'
-//const mongoUrl
-
+const mongoUri = 'mongodb+srv://raiquon111:dynjos-nehfuJ-0pepso@cluster0.yxgz9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const mongoClient = new MongoClient(mongoUri)
 
 
 
@@ -26,13 +26,13 @@ app.use(favicon(path.join(__dirname, 'public','images','favicon_io', 'favicon.ic
 app.use(logger('dev'))
 app.use(express.static('public'))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.urlencoded({ extended: true}))
+//app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false}))
 
 //Routes
 app.get('/', (req, res) => {
     
-    getNasaData((data, isError)=>{
+    getNasaData((data, isError) => {
         if (isError) {
             res.sendStatus(404)
         }else{
@@ -43,20 +43,53 @@ app.get('/', (req, res) => {
     })
 })
 
-app.get('/login', (req, res) =>{
+app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
-
-app.post('/login', (req, res) =>{
-
+//H
+app.post('/login', (req, res) => { 
+    
 })
 
 app.get('/register', (req, res) =>{
     res.render('register.ejs')
 })
 
-app.post('/register', (req, res) =>{
-    
+app.post('/register', async (req, res) =>{
+ 
+    try {
+        const hashedPassword =  await bcrypt.hash(req.body.password, 10)
+
+        try {
+
+            await mongoClient.connect()
+            console.log('___DATABASE CONNECTION SUCCESSFUL___\n')
+
+        }catch(e) {
+            console.log('ERROR:___DATABASE CONNECTION UNSUCCESSFUL___\n')
+            console.log(e)
+        }
+        try {
+            await mongoClient.db("userDatabase").collection("users").insertOne({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                username: req.body.username,
+                password: hashedPassword
+            })
+            console.log('USER ' + req.body.username + ' HAS BEEN REGISTERED' )
+        }catch(e) {
+            console.log(e)
+        }
+        finally {
+            res.redirect('/login')
+        }   
+        
+        
+    }
+    catch(e) {
+        console.log(e)
+    }
 })
 
 
@@ -82,5 +115,8 @@ function getNasaData(callback) {
     
 }
 
+async function registerUser() {
+    
+}
 
 app.listen(port)
